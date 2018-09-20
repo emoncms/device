6793491c7f805cefd7a9180da54e297ad59262dc
TrystanLea <glyn.hudson@openenergymonitor.org>
2018-09-20 09:54:19 +0200

    remove default from text datatype

--- device_schema.php
+++ device_schema.php
@@ -4,8 +4,8 @@ $schema['device'] = array(
     'id' => array('type' => 'int(11)', 'Null'=>false, 'Key'=>'PRI', 'Extra'=>'auto_increment'),
     'userid' => array('type' => 'int(11)'),
     'nodeid' => array('type' => 'text'),
-    'name' => array('type' => 'text', 'default'=>''),
-    'description' => array('type' => 'text','default'=>''),
+    'name' => array('type' => 'text'),
+    'description' => array('type' => 'text'),
     'type' => array('type' => 'varchar(32)'),
     'devicekey' => array('type' => 'varchar(64)'),
     'time' => array('type' => 'int(10)')
