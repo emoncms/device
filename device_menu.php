7ade57afd3e3563dbd3a93e3572798a31578e56b
Emrys <emrys@openenergymonitor.org>
2019-02-15 12:06:09 +0000

    modified menu structure to work with new sidebar

--- device_menu.php
+++ device_menu.php
@@ -1,8 +1,24 @@
 <?php
 
+$menu['setup'][] = array(
+    'text' => _("Device Setup"),
+    'path' => 'device/view',
+    'icon' => 'device',
+    'sort' => 4
+);
+
+/*
+    // no reason to have list of devices in menu. requires list view to edit/add/delete
+    //
     $domain = "messages";
     bindtextdomain($domain, "Modules/device/locale");
     bind_textdomain_codeset($domain, 'UTF-8');
+
+
+    global $mysqli, $redis, $route, $session;
+
+    require_once "Modules/device/device_model.php";
+    $device = new Device($mysqli, $redis);
 
     $menu_dropdown_config[] = array(
             'name'=> dgettext($domain, "Device Setup"),
@@ -11,3 +27,12 @@
             'session'=>"write",
             'order' => 45
     );
+
+    foreach($device->get_list($session['userid']) as $key=>$item) {
+        $sidebar['device'][] = array(
+            'text' => sprintf('%s: %s',$item['nodeid'],$item['name']),
+            'path' => 'device/view',
+            'sort' => $key
+        );
+    }
+*/
