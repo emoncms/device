ed480a9b8f3dc5dfc3adab662f9176700532ccf1
Adrian Minde <Adrian_Minde@live.de>
2019-02-18 14:27:00 +0100

    Fix menu indent

--- device_menu.php
+++ device_menu.php
@@ -5,17 +5,17 @@
     bind_textdomain_codeset($domain, 'UTF-8');
 
     $menu_dropdown_config[] = array(
-            'name'=> dgettext($domain, "Device Setup"),
-            'icon'=>'icon-home',
-            'path'=>"device/view" ,
-            'session'=>"write",
-            'order' => 45
+        'name'=> dgettext($domain, "Device Setup"),
+        'icon'=>'icon-home',
+        'path'=>"device/view" ,
+        'session'=>"write",
+        'order' => 45
     );
 
     $menu_dropdown[] = array(
-            'name'=> dgettext($domain, "Things"),
-            'icon'=>'icon-tasks',
-            'path'=>"device/thing/view" ,
-            'session'=>"write",
-            'order' => 10
+        'name'=> dgettext($domain, "Things"),
+        'icon'=>'icon-tasks',
+        'path'=>"device/thing/view" ,
+        'session'=>"write",
+        'order' => 10
     );
