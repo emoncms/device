e4f0785b69c616d953f683608e9fa22bf1904ae8
Emrys <emrys@openenergymonitor.org>
2019-03-27 12:06:17 +0000

    removed unrequired menu items

--- device_menu.php
+++ device_menu.php
@@ -5,33 +5,3 @@ $menu['sidebar']['setup'][] = array(
     'path' => 'device/view',
     'icon' => 'device'
 );
-
-/*
-    // no reason to have list of devices in menu. requires list view to edit/add/delete
-    //
-    $domain = "messages";
-    bindtextdomain($domain, "Modules/device/locale");
-    bind_textdomain_codeset($domain, 'UTF-8');
-
-
-    global $mysqli, $redis, $route, $session;
-
-    require_once "Modules/device/device_model.php";
-    $device = new Device($mysqli, $redis);
-
-    $menu_dropdown_config[] = array(
-            'name'=> dgettext($domain, "Device Setup"),
-            'icon'=>'icon-home',
-            'path'=>"device/view" ,
-            'session'=>"write",
-            'order' => 45
-    );
-
-    foreach($device->get_list($session['userid']) as $key=>$item) {
-        $sidebar['device'][] = array(
-            'text' => sprintf('%s: %s',$item['nodeid'],$item['name']),
-            'path' => 'device/view',
-            'sort' => $key
-        );
-    }
-*/
