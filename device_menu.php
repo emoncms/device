c09e7bc1f497605fef9e5614004f49180dc3fbbe
Adrian Minde <Adrian_Minde@live.de>
2019-06-20 22:15:47 +0200

    Fix thing menu entry

--- device_menu.php
+++ device_menu.php
@@ -6,8 +6,9 @@ $menu['setup'][] = array(
     'icon' => 'device'
 );
 
-$menu['sidebar'][] = array(
-    'name'=> _("Things"),
-    'path'=>"device/thing/view",
-    'icon'=>'icon-tasks'
+$menu['sidebar']['emoncms'][] = array(
+    'text' => _("Things"),
+    'path' => 'device/thing/view',
+    'icon' => 'device',
+    'order' => 0
 );
