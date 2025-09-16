45b69083a7905ca65e18237430c81c9f6507195a
Trystan Lea <trystan.lea@gmail.com>
2025-09-16 12:23:20 +0100

    restricted access

--- Views/device_api.php
+++ Views/device_api.php
@@ -1,4 +1,7 @@
-<?php global $path, $session, $user; ?>
+<?php 
+defined('EMONCMS_EXEC') or die('Restricted access');
+global $path, $session, $user; 
+?>
 <style>
     a.anchor {
         display: block;
