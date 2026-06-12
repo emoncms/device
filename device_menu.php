8ab5501e7b9d4151f800351225d1173f756aba94
Trystan Lea <trystan.lea@gmail.com>
2026-06-12 10:45:47 +0100

    add divider after device module in menu

--- device_menu.php
+++ device_menu.php
@@ -12,5 +12,13 @@ if ($session["write"]) {
             "order"=>6, 
             "icon"=>"device"
         );
+
+
+        $menu["setup"]["l2"]['divider'] = array(
+            "divider"=>"15px",
+            "href"=>'',
+            "order"=>6
+        );
+
     }
 }
